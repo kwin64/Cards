2a294d06d35964bbe4ba49ce817f125abe34d28a
kwin64 <kwin11bvb@yandex.by>
2021-05-24 20:22:28 +0300

    create navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>loginization</li>
+                <li>Registration</li>
+                <li>Profile</li>
+                <li>404</li>
+                <li>Password recovery</li>
+                <li>Entering the password</li>
+                <li>Test</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
